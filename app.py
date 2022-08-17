4152636305d6bac2d04bca8bb020a5451c0e9927
AntimatterFx <tafimul1998@gmail.com>
2022-08-16 20:51:10 -0500

    Nice

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 app=Flask(__name__)
+app.config['CKEDITOR_SERVE_LOCAL'] = True
 app.config["CKEDITOR_PKG_TYPE"]='basic'
 app.config["SQLALCHEMY_DATABASE_URI"]='sqlite:///app.db'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]=False
